e3e6f089836b0e9c54b063db1a2260e0e03890f0
VQSHR <stoneliuicloud@icloud.com>
2022-11-17 17:10:03 -0500

    createCube error fix

--- Lab3/RigidBody.cpp
+++ Lab3/RigidBody.cpp
@@ -6,7 +6,7 @@ void RigidBody::move()
 
 bool RigidBody::createCube(glm::vec3 position, glm::vec3 linearVelocity, glm::vec3 rotation, glm::vec3 rotationVelocity, GLfloat scale, GLfloat mass, GLfloat volume, GLfloat density, GLfloat restitution, GLfloat friction)
 {
-	RigidBody()
+	RigidBody(position, linearVelocity, rotation, rotationVelocity, scale, mass, volume, density, restitution, friction, shapeType);
 	return false;
 }
 
