fa3ea9d7cdf9ab9ff54e579fb89ba4a4a46842cd
VQSHR <stoneliuicloud@icloud.com>
2022-12-14 17:38:59 -0500

    lab4

---
+++ Lab4/MyMath.h
@@ -0,0 +1,141 @@
+#include <glad/glad.h>
+#include <gl/GL.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/constants.hpp>
+
+class MyUtil {
+public:
+	static glm::mat4 translate(const glm::mat4& m, const glm::vec3& v) {
+		glm::mat4 result = m;
+		
+		result[3] = m[0] * v[0] + m[1] * v[1] + m[2] * v[2] + m[3];
+		return result;
+	}
+
+	static glm::mat4 scale(const glm::mat4& m, const glm::vec3& v) {
+		glm::mat4 result = m;
+		result[0] = m[0] * v[0];
+		result[1] = m[1] * v[1];
+		result[2] = m[2] * v[2];
+		result[3] = m[3];
+		return result;
+	}
+
+	static glm::mat4 rotate(const glm::mat4& m, const glm::vec3& v) {
+
+	}
+
+	static GLfloat catmullRom(GLfloat p0, GLfloat p1, GLfloat p2, GLfloat p3, GLfloat t, GLboolean tan = false) {
+		GLfloat MArray[16] = {
+			-0.5,  1.5, -1.5,  0.5,
+			 1.0, -2.5,  2.0, -0.5,
+			-0.5,  0.0,  0.5,  0.0,
+			 0.0,  1.0,  0.0,  0.0
+		};
+		glm::vec4 T;
+		if (!tan) {
+			GLdouble t2 = t * t;
+			GLdouble t3 = t2 * t;
+			T = glm::vec4(t3, t2, t, 1);
+		}
+		else {
+			T = glm::vec4(3 * t * t, 2 * t, 1, 0);
+		}
+
+		glm::mat4 M = glm::transpose(glm::make_mat4(MArray));
+		glm::vec4 P(p0, p1, p2, p3);
+		GLfloat result = glm::dot(T * M, P);
+		return result;
+	}
+
+	static GLfloat bSpline(GLfloat p0, GLfloat p1, GLfloat p2, GLfloat p3, GLfloat t, GLboolean tan = false) {
+		GLfloat MArray[16] = {
+			-1 / 6.0,  3 / 6.0, -3 / 6.0, 1 / 6.0,
+			 3 / 6.0, -6 / 6.0,  3 / 6.0,       0,
+			-3 / 6.0,        0,  3 / 6.0,       0,
+			 1 / 6.0,  4 / 6.0,  1 / 6.0,       0
+		};
+		glm::vec4 T;
+		if (!tan) {
+			GLdouble t2 = t * t;
+			GLdouble t3 = t2 * t;
+			T = glm::vec4(t3, t2, t, 1);
+		}
+		else {
+			T = glm::vec4(3 * t * t, 2 * t, 1, 0);
+		}
+		glm::mat4 M = glm::transpose(glm::make_mat4(MArray));
+		glm::vec4 P(p0, p1, p2, p3);
+
+		GLfloat result = glm::dot(T * M, P);
+		return result;
+	}
+
+	static GLfloat vector2angle(GLfloat z, GLfloat x)
+	{
+		return glm::atan(z, x);
+	}
+
+	static glm::quat quatMul(glm::quat q1, glm::quat q2) {
+		glm::quat q;
+		GLfloat w1 = q1.w;
+		GLfloat w2 = q2.w;
+		glm::vec3 v1(q1.x, q1.y, q1.z);
+		glm::vec3 v2(q2.x, q2.y, q2.z);
+
+		q.w = w1 * w2 - glm::dot(v1, v2);
+		glm::vec3 v = w1 * v2 + w2 * v1 + glm::cross(v1, v2);
+		q.x = v.x;
+		q.y = v.y;
+		q.z = v.z;
+		return q;
+	}
+
+	static glm::quat euler2quat(glm::vec3 eulerAngles)
+	{
+		GLfloat x = eulerAngles.x * 0.5;
+		GLfloat y = eulerAngles.y * 0.5;
+		GLfloat z = eulerAngles.z * 0.5;
+
+		glm::quat qz, qy, qx;
+		qz = glm::quat(glm::cos(z), 0, 0, glm::sin(z));
+		qy = glm::quat(glm::cos(y), 0, glm::sin(y), 0);
+		qx = glm::quat(glm::cos(x), glm::sin(x), 0, 0);
+
+		glm::quat q = quatMul(quatMul(qz, qy), qx);
+		return q;
+	}
+
+	static glm::mat4 quat2mat4(glm::quat q) {
+		GLfloat w = q.w;
+		GLfloat x = q.x;
+		GLfloat y = q.y;
+		GLfloat z = q.z;
+
+		GLfloat x2 = x * x;
+		GLfloat y2 = y * y;
+		GLfloat z2 = z * z;
+
+		GLfloat mat4array[16] = {
+			1 - 2 * y2 - 2 * z2,   2 * x * y - 2 * w * z,   2 * x * z + 2 * w * y, 0,
+			  2 * x * y + 2 * w * z, 1 - 2 * x2 - 2 * z2,   2 * y * z - 2 * w * x, 0,
+			  2 * x * z - 2 * w * y,   2 * y * z + 2 * w * x, 1 - 2 * x2 - 2 * y2, 0,
+						  0,               0,               0, 1
+		};
+
+		return glm::transpose(glm::make_mat4(mat4array));
+	}
+
+	static GLfloat lerp(GLfloat p0, GLfloat p1, GLfloat t) {
+		GLfloat MArray[4] = { -1, 1, 1, 0 };
+		glm::vec2 T(t, 1);
+		glm::mat2 M = glm::transpose(glm::make_mat2(MArray));
+		glm::vec2 P(p0, p1);
+		GLfloat result = glm::dot(T * M, P);
+		return result;
+	}
+
+};
---
+++ Lab4/RigidBody.cpp
@@ -0,0 +1,129 @@
+#include "RigidBody.h"
+
+
+RigidBody::RigidBody(
+	glm::vec3 position,
+	glm::vec3 linearVelocity,
+	glm::vec3 rotation,
+	glm::vec3 rotationVelocity,
+	glm::vec3 force,
+	GLfloat mass,
+	GLfloat restitution,
+	GLfloat friction)
+{
+	this->position = position;
+	this->linearVelocity = linearVelocity;
+	this->rotation = rotation;
+	this->rotationVelocity = rotationVelocity;
+	this->force = force;
+	this->mass = mass;
+	this->restitution = restitution;
+	this->friction = friction;
+}
+
+void RigidBody::update(GLfloat dt)
+{
+	linearVelocity += force * dt;
+	position += linearVelocity * dt;
+	rotation += rotationVelocity * dt;
+	force = glm::vec3(0, -9.8, 0) * mass;
+}
+
+void RigidBody::move(glm::vec3 amount)
+{
+	position += amount;
+}
+
+void RigidBody::setPosition(glm::vec3 position)
+{
+	this->position = position;
+}
+
+void RigidBody::applyForce(glm::vec3 force)
+{
+	this->force += force;
+}
+
+Sphere::Sphere(glm::vec3 position,
+	glm::vec3 linearVelocity,
+	glm::vec3 rotation,
+	glm::vec3 rotationVelocity,
+	glm::vec3 force,
+	GLfloat mass,
+	GLfloat restitution,
+	GLfloat friction,
+	GLfloat radius)
+	: RigidBody(position,
+		linearVelocity,
+		rotation,
+		rotationVelocity,
+		force,
+		mass,
+		restitution,
+		friction), radius(radius) {}
+
+bool Sphere::intersect(Sphere a, Sphere b, glm::vec3& normal, GLfloat& depth)
+{
+	normal = glm::vec3(0.0);
+	depth = 0.0f;
+
+	GLfloat distance = glm::distance(a.position, b.position);
+	GLfloat radii = a.radius + b.radius;
+
+	if (distance >= radii)
+	{
+		return false;
+	}
+
+	normal = glm::normalize(a.position - b.position);
+	depth = radii - distance;
+
+	return true;
+}
+
+bool Sphere::intersectBound(glm::vec3& normal, glm::vec3& depth)
+{
+	normal = glm::vec3(0.0);
+	depth = glm::vec3(0.0);
+
+	bool isHit = false;
+
+	GLfloat dist;
+	dist = position.x - (-15) - radius;
+	if (dist < 0) {
+		isHit = true;
+		normal.x += 1;
+		depth.x += dist; 
+	}
+	dist = 15 - position.x - radius;
+	if (dist < 0) {
+		isHit = true;
+		normal.x -= 1;
+		depth.x += dist; 
+	}
+	dist = position.y - 0 - radius;
+	if (dist < 0) {
+		isHit = true;
+		normal.y += 1;
+		depth.y += dist; 
+	}
+	dist = 30 - position.y - radius;
+	if (dist < 0) {
+		isHit = true;
+		normal.y -= 1;
+		depth.y += dist;
+	}
+	dist = position.z - (-15) - radius;
+	if (dist < 0) {
+		isHit = true;
+		normal.z += 1;
+		depth.z += dist; 
+	}
+	dist = 15 - position.z - radius;
+	if (dist < 0) {
+		isHit = true;
+		normal.z -= 1;
+		depth.z += dist;
+	}
+	return isHit;
+}
